42f1cf2926c585949ac707656a861c1ffa2e7b4e
Colin <higgins.colin@gmail.com>
2018-06-20 00:59:29 -0500

    gather list of places within 10m

--- IG_collect2.py
+++ IG_collect2.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import time
 
 keys = []
 inFile = open("/home/colin/Desktop/GEO_IG/keys", "r")
@@ -31,16 +32,44 @@ def get_user_id(insta_username):
 
 #id_var = get_user_id('colin_higgins911')
 
-def get_loc_search(lat, lng):
+def get_loc_search(lat, lng, distance):
 
     request_url = (base_url + 'locations/search?lat=%s&lng=%s&access_token=%s') % (lat, lng, access_token)
     print 'GET REQUEST URL : %s' % (request_url)
 
     out_data = requests.get(request_url).json()
+    product = []
+    i = 0
 
     if out_data['meta']['code'] == 200:
         if len(out_data['data']):
-            return out_data['data'][0]['name']
+                #here's where most of the magic happens
+                try:
+                    while i < (len(out_data['data'])+1):
+                        i += 1
+                        #collecting all the data
+                        #product = an individual location
+                        product_id = out_data['data'][i]['id'].encode("utf-8")
+                            
+                        product_lat = out_data['data'][i]['latitude']
+                        product_long = out_data['data'][i]['longitude']
+
+                        #tuple the GPS for ease of use later, incase we need to
+                        geo_tup= (product_lat, product_long)
+                            
+                        product_name = out_data['data'][i]['name'].encode("utf-8")
+
+                        temp = []
+                        temp.append(product_id)
+                        temp.append(product_lat)
+                        temp.append(product_long)
+                        temp.append(product_name)
+                    
+                        product.append(temp)
+
+                except:
+                    print("Done!")
+
         else:
             return None
 
@@ -50,13 +79,16 @@ def get_loc_search(lat, lng):
 
         quit()
 
+    return(product)
+
 def main():
     # lat = (input("enter LATITUDE"))
     # lng = (input("enter LONGITUDE"))
 
     lat = 30.2672
     lng = -97.74131
+    distance = 10
 
-    print(get_loc_search(lat, lng))
+    print(get_loc_search(lat, lng, distance))
 
 main()
