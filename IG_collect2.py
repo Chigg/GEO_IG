6c56b30ce760f251a401e792fd29166a08b783d3
Colin <higgins.colin@gmail.com>
2018-06-20 16:05:15 -0500

    finished & example

--- IG_collect2.py
+++ IG_collect2.py
@@ -1,6 +1,7 @@
 import requests
 import sys
-import time
+import random
+import csv
 
 keys = []
 inFile = open("/home/colin/Desktop/GEO_IG/keys", "r")
@@ -15,7 +16,7 @@ base_url = 'https://api.instagram.com/v1/'
 
 def get_loc_search(lat, lng, distance):
 
-    request_url = (base_url + 'locations/search?lat=%s&lng=%s&access_token=%s') % (lat, lng, access_token)
+    request_url = (base_url + 'locations/search?lat=%s&lng=%s&distance=%s&access_token=%s') % (lat, lng, distance, access_token)
     print 'GET REQUEST URL : %s' % (request_url)
 
     out_data = requests.get(request_url).json()
@@ -90,8 +91,27 @@ def media_location_info(location):
     out_data = requests.get(request_url).json()
 
     if out_data['meta']['code'] == 200:
-       # print(out_data['data'][0]['likes']['count'])
-        print(out_data['data'])
+        #since the app is sandboxed we can only look at
+        #this data for self and users within the sandbox (up to 10)
+        #print(out_data['data'][0]['likes']['count'])
+        
+        #so we need to simulate likes for local media.
+        seed = random.randint(0, 300)
+        seed_min = (seed - random.randint(0,50))
+        seed_max = (seed + random.randint(0,50))
+        i = 0
+        sim_likes = []
+
+        while i <= 25:
+            sim_likes.append(random.randint(seed_min, seed_max))
+            
+            i += 1
+
+        like_avg = sum(sim_likes) / len(sim_likes)
+            
+        location.append(like_avg)
+        return(location)
+
     else:
         print( 'STATUS CODE OTHER THAN 200 RECIEVED!')
         print(out_data['meta']['error_type'])
@@ -102,11 +122,22 @@ def main():
 
     lat = 30.2672
     lng = -97.74131
-    distance = 1
+    distance = 10
 
     loc_list = (get_loc_search(lat, lng, distance))
+    finished = []
+    print("writing to analysis.csv")
+    heading = ['ID', 'LATITUDE', 'LONGITUDE', 'AVG_LIKES']
+
+    with open("analysis.csv", 'a') as endfile:
+        out_writer = csv.writer(endfile, delimiter = '|')
+        out_writer.writerow(heading)
+
+        for loc in loc_list:
+            row = (media_location_info(loc))
+            out_writer.writerow(row)
+            finished.append(row)
+
+    print(finished)
     
-    for loc in loc_list:
-        media_location_info(loc)
-        break
 main()
