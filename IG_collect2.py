bc016413c7558ae064ef77e14d24e2fd86c6762a
Colin <higgins.colin@gmail.com>
2018-06-20 01:42:48 -0500

    check recent media by loc

--- IG_collect2.py
+++ IG_collect2.py
@@ -13,25 +13,6 @@ client_secret = str(keys[1])
 
 base_url = 'https://api.instagram.com/v1/'
 
-def get_user_id(insta_username):
-    request_url = (base_url + 'users/search?q=%s&access_token=%s') % (insta_username, access_token)
-    print 'GET REQUEST URL : %s' % (request_url)
-
-    user_info = requests.get(request_url).json()
-
-    if user_info['meta']['code'] == 200:
-        if len(user_info['data']):
-            return user_info['data'][0]['id']
-        else:
-            return None
-    else:
-        print( 'STATUS CODE OTHER THAN 200 RECIEVED!')
-        print(user_info['meta']['error_type'])
-
-        quit()
-
-#id_var = get_user_id('colin_higgins911')
-
 def get_loc_search(lat, lng, distance):
 
     request_url = (base_url + 'locations/search?lat=%s&lng=%s&access_token=%s') % (lat, lng, access_token)
@@ -81,14 +62,51 @@ def get_loc_search(lat, lng, distance):
 
     return(product)
 
+def location_info(location):
+  
+    loc_id = location[0]
+    request_url = (base_url + 'locations/%s/media/recent?access_token=%s') % (loc_id, access_token)
+    print 'GET REQUEST URL : %s' % (request_url)
+    
+    out_data = requests.get(request_url).json()
+
+    if out_data['meta']['code'] == 200:
+       # print(out_data['data'][0]['likes']['count'])
+        print(out_data['data'])
+    else:
+        print( 'STATUS CODE OTHER THAN 200 RECIEVED!')
+        print(out_data['meta']['error_type'])
+
+
+def media_location_info(location):
+  
+    loc_id = location[0]
+    loc_lat = location[1]
+    loc_lng = location[2]
+
+    request_url = (base_url + 'media/search?lat=%s&lng=%s&distance=1000&access_token=%s') % (loc_lat, loc_lng, access_token)
+    print 'GET REQUEST URL : %s' % (request_url)
+    
+    out_data = requests.get(request_url).json()
+
+    if out_data['meta']['code'] == 200:
+       # print(out_data['data'][0]['likes']['count'])
+        print(out_data['data'])
+    else:
+        print( 'STATUS CODE OTHER THAN 200 RECIEVED!')
+        print(out_data['meta']['error_type'])
+    
 def main():
     # lat = (input("enter LATITUDE"))
     # lng = (input("enter LONGITUDE"))
 
     lat = 30.2672
     lng = -97.74131
-    distance = 10
+    distance = 1
 
-    print(get_loc_search(lat, lng, distance))
-
+    loc_list = (get_loc_search(lat, lng, distance))
+    
+    for loc in loc_list:
+        media_location_info(loc)
+        break
 main()
