67b112e482b4f16ef97c7a4eea9fc2af165294db
Colin <higgins.colin@gmail.com>
2018-06-22 03:38:33 -0500

    output examples

--- IG_collect2.py
+++ IG_collect2.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import random
 import csv
+from operator import itemgetter
 
 keys = []
 inFile = open("./keys", "r")
@@ -15,7 +16,7 @@ client_secret = str(keys[1])
 base_url = 'https://api.instagram.com/v1/'
 
 def get_loc_search(lat, lng, distance):
-
+    distance = 10
     request_url = (base_url + 'locations/search?lat=%s&lng=%s&distance=%s&access_token=%s') % (lat, lng, distance, access_token)
     print 'GET REQUEST URL : %s' % (request_url)
 
@@ -85,12 +86,16 @@ def media_location_info(location):
     loc_lat = location[1]
     loc_lng = location[2]
 
-    request_url = (base_url + 'media/search?lat=%s&lng=%s&distance=1000&access_token=%s') % (loc_lat, loc_lng, access_token)
+    request_url = (base_url + 'media/search?lat=%s&lng=%s&distance=100&access_token=%s') % (loc_lat, loc_lng, access_token)
     print 'GET REQUEST URL : %s' % (request_url)
     
-    out_data = requests.get(request_url).json()
+    #out_data = requests.get(request_url).json()
+    #you'd use below if you weren't sandboxed
+    #if out_data['meta']['code'] == 200:
 
-    if out_data['meta']['code'] == 200:
+    sandboxed = True
+    if sandboxed:
+        print("FAKE QUERY")
         #since the app is sandboxed we can only look at
         #this data for self and users within the sandbox (up to 10)
         #print(out_data['data'][0]['likes']['count'])
@@ -126,13 +131,35 @@ def main():
     temp_list = (get_loc_search(lat, lng, distance))
     loc_list = []
 
+    #test
+    loc_list.append(temp_list[0])
+
     #need to fix this loop to ignore duplicates
-    for x in range(0,10):
+    for x in range(0,30):
 
         next_lat = temp_list[len(temp_list)-1][1]
         next_lng = temp_list[len(temp_list)-1][2]
         for loc in temp_list:
-            loc_list.append(loc)
+            #sort
+            i = 0
+            print("step 0")
+            if len(loc_list):
+                print("step 1")
+                while i < len(loc_list):
+                    #below line prints the ID
+                    #print(loc_list[i][0])
+                    print("step 2")
+                    #if new location == location already in list
+                    if loc[0] == loc_list[i][0]:
+                        #terminate the loop and don't append it to loc_list
+                        print("DUPLICATE")
+                        break
+    
+                    if i == len(loc_list)-1:
+                        print(loc_list)
+                        loc_list.append(loc)
+
+                    i += 1
 
         temp_list = (get_loc_search(next_lat, next_lng, distance))
    
@@ -140,7 +167,7 @@ def main():
     print("writing to analysis.csv")
     heading = ['ID', 'LATITUDE', 'LONGITUDE', 'NAME', 'AVG_LIKES']
 
-    with open("analysis.csv", 'a') as endfile:
+    with open("analysis.csv", 'w') as endfile:
         out_writer = csv.writer(endfile, delimiter = '|')
         out_writer.writerow(heading)
 
